92a0a967343f11e0bdc0e7a15b0640bd733cf180
Mohit Pandey <mp7698921@gmail.com>
2024-03-19 14:10:42 +0530

    static issue fix

--- Courses_App/urls.py
+++ Courses_App/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
