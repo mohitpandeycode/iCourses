620051c559dd15ad906fb5a07a9ded77b22fa3f4
Mohit Pandey <mp7698921@gmail.com>
2024-03-19 16:08:49 +0530

    final host

--- Courses_App/settings.py
+++ Courses_App/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(a39tri0d23to4(+1ni(=m6yepoaxnay*!8wyv&zbkhe(@lq2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
