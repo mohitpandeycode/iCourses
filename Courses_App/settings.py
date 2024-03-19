56ebddb279e33de71dfa3e3b954b168c39d4e3c4
Mohit Pandey <mp7698921@gmail.com>
2024-03-19 13:58:46 +0530

    database issue fix

--- Courses_App/settings.py
+++ Courses_App/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'Courses_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "Mohit@1234#",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
