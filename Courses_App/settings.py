fb50736f36904a891e79f6504849a3e7cebf3370
Mohit Pandey <mp7698921@gmail.com>
2024-03-19 16:34:13 +0530

    host to render

--- Courses_App/settings.py
+++ Courses_App/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(a39tri0d23to4(+1ni(=m6yepoaxnay*!8wyv&zbkhe(@lq2*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
